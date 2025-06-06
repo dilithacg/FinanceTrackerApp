e323bb0ffefb6f8b91785052176d5e399ab545ce
dilitha ganegama <dilithacg05@gmail.com>
2025-06-06 15:41:44 +0530

    final

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
 import '../bottom_nav_bar_screen.dart';
 import '../screens/auth/login_screen.dart';
 import '../screens/auth/register_screen.dart';
 
-
 final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+  redirect: (BuildContext context, GoRouterState state) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    final loggingIn = state.uri.path == '/';
+
+    // Redirect logged-in user from login to home
+    if (user != null && loggingIn) return '/home';
+
+    // Redirect non-logged-in user trying to access home
+    if (user == null && state.uri.path == '/home') return '/';
+
+    // No redirection
+    return null;
+  },
   routes: [
     GoRoute(
       path: '/',
